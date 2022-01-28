81cca5d2643407ec4e56c0344213c79859ad8e3d
Shashank <95507046+SDK2575@users.noreply.github.com>
2022-01-27 19:29:10 -0500

    Add files via upload

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetSingleton();
+
+            Singleton singleton2 = Singleton.GetSingleton();
+
+            Singleton singleton3 = Singleton.GetSingleton();
+
+            Singleton singleton4 = Singleton.GetSingleton();
+
+            Singleton singleton5 = Singleton.GetSingleton();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    //Can not inherit 
+    sealed class Singleton
+    {
+
+        private static Singleton obj = null;
+
+        private static int counter = 0;
+
+        //private constructor 
+        //can not create instance directly 
+        private Singleton()
+        {
+            
+        }
+
+        /// <summary>
+        /// Create multiple instances using singleton 
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetSingleton()
+        {
+
+            counter++;
+            
+            if (counter <=  5)
+            {
+                obj = new Singleton();
+                Console.WriteLine("Instance :" + counter + " created");
+            }
+            
+            return obj;
+
+        }
+
+    }
+}
