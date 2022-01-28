42083920749f98806c05401f07ac566e17128423
Shashank <95507046+SDK2575@users.noreply.github.com>
2022-01-27 19:27:36 -0500

    Add files via upload

---
+++ FactoryLayer/FactoryMiddleLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceLayer;
+using MiddleLayer;
+using InterfaceDal;
+
+namespace FactoryLayer
+{
+    public class FactoryCustomer
+    {
+        List<ICustomer> custs = null;
+        public FactoryCustomer()
+        {
+
+            if (custs == null)
+            {
+                custs = new List<ICustomer>();
+                custs.Add(new SimpleCustomer());
+                custs.Add(new CustomerSpecial());
+            }
+        }
+        public ICustomer Create(int type)
+        {
+            return  custs[type].Clone();
+            
+        }
+        
+    }
+}
